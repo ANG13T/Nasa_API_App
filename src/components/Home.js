3eb86023d23c03dec909d6f5d7f6c8a827ea1f5f
angelina-tsuboi <angelina.t1832@gmail.com>
2020-01-05 20:58:31 -0800

    Switched API

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,12 +8,12 @@ class Home extends React.Component {
     }
 
     componentDidMount(){
-        axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=hzYniRWWJjQwKm1nt8fKGGv7gX0BF8Iq6AKC8fNz')
+        axios.get('https://api.nasa.gov/EPIC/api/natural/images?api_key=hzYniRWWJjQwKm1nt8fKGGv7gX0BF8Iq6AKC8fNz')
         .then(res => {
             console.log(res)
-            console.log(res.data.photos)
+            console.log(res.data)
             this.setState({
-                posts: res.data.photos.slice(0,20)
+                posts: res.data.slice(0,20)
             })
         })
     }
@@ -23,13 +23,13 @@ class Home extends React.Component {
         const postList = posts.length ? (
             posts.map(post => {
                 return(
-                    <div className="post card" key={post.id}>
+                    <div className="post card" key={post.identifier}>
                         <div className="card-content">
-                            <Link to={'/' + post.id}>
+                            <Link to={'/' + post.identifier}>
                             
                             </Link>
-                            <span className="card-title">{post.camera.full_name}</span>
-                            <img class="materialboxed" width="650" src={post.img_src} />
+                            <span className="card-title">{post.caption}</span>
+                            <img className="materialboxed" width="650" src={post.image} />
                             <p>{post.earth_date}</p>
                         </div>
                     </div>
